dde4f5771ec16edb67636a797ba72b812a31b674
Bartosz Smalcerz <b.smalcerz@gmail.com>
2019-10-28 11:38:01 +0100

    modived html, new style, new js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('hello world');
 
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section');
+    
+    document.addEventListener('mousewheel', function(event) {
+        console.log(event.wheelDelta)
+    })
+});
