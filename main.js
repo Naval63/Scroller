5a0841a715fff9375d74c5171bf4ed1bb22e28db
Bartosz Smalcerz <b.smalcerz@gmail.com>
2019-10-28 12:27:33 +0100

    main.js section index

--- main.js
+++ main.js
@@ -3,8 +3,21 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const rootElement = document.querySelector('#root');
     const sections = document.querySelectorAll('section');
+    let currentSectionIndex = 0;
     
     document.addEventListener('mousewheel', function(event) {
-        console.log(event.wheelDelta)
+        const direction = event.wheelDelta < 0 ? 1 : -1;
+        
+        if(direction === 1) {
+            const isLastSection = currentSectionIndex === sections.length -1;
+            if(isLastSection) return;
+        } else if(direction === -1) {
+            const firstSection = currentSectionIndex === 0;
+            if(firstSection) return;
+        }
+
+        currentSectionIndex = currentSectionIndex + direction;
+        console.log(currentSectionIndex)
+
     })
 });
